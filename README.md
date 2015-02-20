12ce8716a7826ed47570714d4b3660afc7469a47
Kyle Fuller <inbox@kylefuller.co.uk>
2015-02-20 12:44:41 +0000

    Add "Deploy to Heroku"

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 [![Apiary Documentation](https://img.shields.io/badge/Apiary-Documented-blue.svg)](http://docs.pollsapi.apiary.io/)
 
+[![Deploy to Heroku](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy?template=https://github.com/apiaryio/polls-api)
+
--- polls/settings.py
+++ polls/settings.py
@@ -1,8 +1,18 @@
+import os
+
+
+def get_env(key, default=True):
+    value = os.environ.get(key, default)
+    return (value == True or value.lower() == 'true' or value == '1' or
+            value.lower() == 'yes')
+
+
 # Enables the ability to create a question
-CAN_CREATE_QUESTION = True
+CAN_CREATE_QUESTION = get_env('POLLS_CAN_CREATE_QUESTION')
 
 # Enables the ability to delete a question
-CAN_DELETE_QUESTION = True
+CAN_DELETE_QUESTION = get_env('POLLS_CAN_DELETE_QUESTION')
 
 # Enables the ability to vote on a question
-CAN_VOTE_QUESTION = True
+CAN_VOTE_QUESTION = get_env('POLLS_CAN_VOTE_QUESTION')
+
