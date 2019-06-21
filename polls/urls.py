18142c70eae77d3bc4c9b30efec01b46dc77f7fb
Kyle Fuller <kyle@fuller.li>
2019-06-21 17:11:50 +0100

    refactor(project): align with default django project

--- polls/urls.py
+++ polls/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import include, url
+from django.conf.urls import url
 from polls.views import (RootResource, QuestionCollectionResource,
                          QuestionResource, ChoiceResource)
 
