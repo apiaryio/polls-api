759848a4331b123baa1889404930e1631ed484d6
Kyle Fuller <kyle@fuller.li>
2019-06-21 17:17:10 +0100

    feat: add /500 to test 500's

--- polls/urls.py
+++ polls/urls.py
@@ -3,9 +3,14 @@ from polls.views import (RootResource, QuestionCollectionResource,
                          QuestionResource, ChoiceResource)
 
 
+def error_view(request):
+    raise Exception('Test exception')
+
+
 urlpatterns = [
     url(r'^$', RootResource.as_view()),
     url(r'^questions$', QuestionCollectionResource.as_view()),
     url(r'^questions/(?P<pk>[\d]+)$', QuestionResource.as_view()),
     url(r'^questions/(?P<question_pk>[\d]+)/choices/(?P<pk>[\d]+)$', ChoiceResource.as_view()),
+    url(r'^500/$', error_view),
 ]
